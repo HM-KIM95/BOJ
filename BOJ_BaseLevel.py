ff905be315a6f7981d9d766122b86f9f1f4e31cb
NIXKim <dbb04017@gmail.com>
2025-11-07 17:47:54 +0900

    Q.1330 Solved

--- BOJ_BaseLevel.py
+++ BOJ_BaseLevel.py
@@ -1,5 +1,13 @@
-print('|\\_/|')
-print('|q p|   /}')
-print('( 0 )"""\\')
-print('|"^"`    |')
-print('||_/=\\\\__|')
+import sys
+input = sys.stdin.readline
+
+A, B = map(int, input().split())
+
+if A > B:
+    print(">")
+    
+elif A < B:
+    print("<")
+    
+elif A == B:
+    print("==")
