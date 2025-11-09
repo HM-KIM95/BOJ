9b8de5a61a9190d200536d76afec8cb11b2cdd1b
NIXKim <dbb04017@gmail.com>
2025-11-09 17:22:00 +0900

    Q.2739 Solved

--- BOJ_BaseLevel.py
+++ BOJ_BaseLevel.py
@@ -1,12 +1,4 @@
-a, b, c = map(int, input().split())
+n = int(input())
 
-if a == b == c :
-    prize = 10000 + a * 1000
-elif a == b or a == c:
-    prize = 1000 + a * 100
-elif b == c:
-    prize = 1000 + b * 100
-else:
-    prize = max(a, b, c) * 100
-
-print(prize)
+for i in range(1, 10):
+    print(f"{n} * {i} = {n * i}")
